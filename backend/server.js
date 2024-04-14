4965fdf8a4b15f06cb28b1f84dad81e3de8cf34a
Umar Jamil <99021340+UMAR-78@users.noreply.github.com>
2024-04-14 19:09:55 +0500

    acha

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
-const express = require("express"); 
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const revenueRoutes = require('./routes/revenueRoutes')
+const connectDB = require('./utils/db')
+const path = require('path')
+connectDB();
 
-app.get("/", (req, res) => 
-{
-   res.send("Express on Vercel"
- );
- });
+app.use(express.json());
+app.use(cors());
 
-const PORT = process.env.PORT || 5000; 
+app.use('/admin/api/v1' , revenueRoutes)
 
-app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+app.get('*' ,(req , res) =>{
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
+
+
+
+app.listen(5000, () => {
+  console.log("App is listening on the port 5000");
+});
