0e8b39e060e587f7854b74567ef12d6e254f4c5f
Umar Jamil <99021340+UMAR-78@users.noreply.github.com>
2024-04-14 18:45:12 +0500

    ok

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,10 @@ app.use(cors());
 
 app.use('/admin/api/v1' , revenueRoutes)
 
-// app.use(express.static(path.join(__dirname, '/frontend/dist')))
-// app.get('*' ,(req , res) =>{
-//   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
-// })
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+app.get('*' ,(req , res) =>{
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
 
 
 
